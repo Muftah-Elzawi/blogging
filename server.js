8c430e1209f024bf9f0a7b87b39c8a017058164e
muftah <muftah.elzawi@gmail.com>
2020-03-21 15:09:21 +0200

    starting the server

--- server.js
+++ server.js
@@ -3,4 +3,25 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+
+//app
+const app = express();
+
+//middlewares 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//cors
+app.use(cors());
+
+//routs
+app.get('/api', (req, res) => {
+    res.json({ 'thanks': 'thanks', time: Date().toString() })
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+}); 
