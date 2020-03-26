d788ebe2f8e68cbe7f75e7b75412c12838d40afd
muftah <muftah.elzawi@gmail.com>
2020-03-26 16:49:41 +0200

    signup Controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,34 @@
+const User = require('../models/user');
+const shortId = require('shortid');
+
 exports.signup = (req, res) => {
     console.log(req.body)
-    const { name, email, password } = req.body
+    User.findOne({ email: req.body.email }).exec((err, user) => {
+        if (user) {
+            return res.status(400).json({
+                error: 'this email is taken'
+            })
+        }
 
-    res.json({
-        user: { name, email, password }
+        const { name, email, password } = req.body
+        let username = shortId.generate();
+        let profile = `${process.env.CLIENT_URL}/profile/${username}`
+
+        let newUser = new User({ name, email, password, profile, username })
+        newUser.save((err, success) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            // res.json({
+            //     user: success
+            // })
+            res.json({
+                message: 'signup success! please signin'
+            })
+
+        })
     })
+
 };
